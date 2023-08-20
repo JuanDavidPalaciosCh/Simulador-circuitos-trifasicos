a084fda1c08f5613f89a73fad514972efc317ea1
JuanDavidPalaciosCh <juandapalaciosch@gmail.com>
2023-08-20 18:59:29 -0500

    Se agrega caso Estrella - Delta, y funciones de cambio de voltaje de delta a estrella y viceversa

--- 3PhaseCircuits.py
+++ 3PhaseCircuits.py
@@ -1,16 +1,23 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
+import time
+from PIL import Image
 from functions import *
+import streamlit as st
+
 
 st.title('Simulador circuitos trifasicos')
 
 option = st.selectbox(
     'Ingrese el circuito trifasico a resolver: ',
-    ('Delta-Delta', 'Estrella-Estrella', 'Delta-Estrella', 'Estrella-Delta'))
+    ('Delta-Delta', 'Estrella-Estrella 4 hilos', 'Delta-Estrella', 'Estrella-Delta'))
 
 
 if option == 'Delta-Delta':
+
+    image = Image.open("DistribucionDeltaDelta.png")
+    st.image(image, caption='Circuito trifasico Delta - Delta')
 
     "## Selección voltajes:"
 
@@ -110,12 +117,14 @@ if option == 'Delta-Delta':
 
     if graphs == 'Corrientes de fase':
 
-        corriente_ab = lambda x: i_fase[0][0] * math.cos(x + i_fase[0][1])
-        corriente_bc = lambda x: i_fase[1][0] * math.cos(x + i_fase[1][1])
-        corriente_ca = lambda x: i_fase[2][0] * math.cos(x + i_fase[2][1])
+        frecuency = st.slider('Seleccione la frecuencia: ', 0.00, 100.00, 50.00)
 
-        rango = st.slider('Seleccione el valor maximo de x: ', 0.00, 50.00, 25.00)
-        step = 0.01
+        corriente_ab = lambda x: i_fase[0][0] * math.cos(math.radians(frecuency * x + i_fase[0][1]))
+        corriente_bc = lambda x: i_fase[1][0] * math.cos(math.radians(frecuency * x + i_fase[1][1]))
+        corriente_ca = lambda x: i_fase[2][0] * math.cos(math.radians(frecuency * x + i_fase[2][1]))
+
+        rango = st.slider('Seleccione el valor maximo de x: ', 0.00, 100.00, 50.00)
+        step = 0.001
 
         i_ab_x = np.arange(0, rango, step)
         i_ab_y = [corriente_ab(i) for i in i_ab_x]
@@ -138,12 +147,15 @@ if option == 'Delta-Delta':
         st.pyplot(fig)
 
     elif graphs == 'Corrientes de linea':
-        corriente_aa = lambda x: i_linea[0][0] * math.cos(x + i_linea[0][1])
-        corriente_bb = lambda x: i_linea[1][0] * math.cos(x + i_linea[1][1])
-        corriente_cc = lambda x: i_linea[2][0] * math.cos(x + i_linea[2][1])
 
-        rango = st.slider('Seleccione el valor maximo de x: ', 0.00, 50.00, 25.00)
-        step = 0.01
+        frecuency = st.slider('Seleccione la frecuencia: ', 0.00, 100.00, 50.00)
+
+        corriente_aa = lambda x: i_linea[0][0] * math.cos(math.radians(frecuency * x + i_linea[0][1]))
+        corriente_bb = lambda x: i_linea[1][0] * math.cos(math.radians(frecuency * x + i_linea[1][1]))
+        corriente_cc = lambda x: i_linea[2][0] * math.cos(math.radians(frecuency * x + i_linea[2][1]))
+
+        rango = st.slider('Seleccione el valor maximo de x: ', 0.00, 100.00, 50.00)
+        step = 0.001
 
         i_aa_x = np.arange(0, rango, step)
         i_aa_y = [corriente_aa(i) for i in i_aa_x]
@@ -166,12 +178,15 @@ if option == 'Delta-Delta':
         st.pyplot(fig)
 
     elif graphs == 'Tensiones de fase':
-        tension_ab = lambda x: v_fase[0][0] * math.cos(x + v_fase[0][1])
-        tension_bc = lambda x: v_fase[1][0] * math.cos(x + v_fase[1][1])
-        tension_ca = lambda x: v_fase[2][0] * math.cos(x + v_fase[2][1])
 
-        rango = st.slider('Seleccione el valor maximo de x: ', 0.00, 50.00, 25.00)
-        step = 0.01
+        frecuency = st.slider('Seleccione la frecuencia: ', 0.00, 100.00, 50.00)
+
+        tension_ab = lambda x: v_fase[0][0] * math.cos(math.radians(frecuency * x + v_fase[0][1]))
+        tension_bc = lambda x: v_fase[1][0] * math.cos(math.radians(frecuency * x + v_fase[1][1]))
+        tension_ca = lambda x: v_fase[2][0] * math.cos(math.radians(frecuency * x + v_fase[2][1]))
+
+        rango = st.slider('Seleccione el valor maximo de x: ', 0.00, 100.00, 50.00)
+        step = 0.001
 
         v_ab_x = np.arange(0, rango, step)
         v_ab_y = [tension_ab(i) for i in v_ab_x]
@@ -194,12 +209,15 @@ if option == 'Delta-Delta':
         st.pyplot(fig)
 
     else:
-        tension_ab = lambda x: v_linea[0][0] * math.cos(x + v_linea[0][1])
-        tension_bc = lambda x: v_linea[1][0] * math.cos(x + v_linea[1][1])
-        tension_ca = lambda x: v_linea[2][0] * math.cos(x + v_linea[2][1])
 
-        rango = st.slider('Seleccione el valor maximo de x: ', 0.00, 50.00, 25.00)
-        step = 0.01
+        frecuency = st.slider('Seleccione la frecuencia: ', 0.00, 100.00, 50.00)
+
+        tension_ab = lambda x: v_linea[0][0] * math.cos(math.radians(frecuency * x + v_linea[0][1]))
+        tension_bc = lambda x: v_linea[1][0] * math.cos(math.radians(frecuency * x + v_linea[1][1]))
+        tension_ca = lambda x: v_linea[2][0] * math.cos(math.radians(frecuency * x + v_linea[2][1]))
+
+        rango = st.slider('Seleccione el valor maximo de x: ', 0.00, 100.00, 50.00)
+        step = 0.001
 
         v_ab_x = np.arange(0, rango, step)
         v_ab_y = [tension_ab(i) for i in v_ab_x]
@@ -222,7 +240,10 @@ if option == 'Delta-Delta':
         st.pyplot(fig)
 
 
-elif option == 'Estrella-Estrella':
+elif option == 'Estrella-Estrella 4 hilos':
+
+    image = Image.open("DistribucionEstrellaEstrella4Hilos.png")
+    st.image(image, caption='Circuito trifasico Estrella - Estrella 4 hilos')
     
     "## Selección voltajes:"
 
@@ -322,12 +343,14 @@ elif option == 'Estrella-Estrella':
 
     if graphs == 'Corrientes de fase':
 
-        corriente_an = lambda x: i_fase[0][0] * math.cos(x + i_fase[0][1])
-        corriente_bn = lambda x: i_fase[1][0] * math.cos(x + i_fase[1][1])
-        corriente_cn = lambda x: i_fase[2][0] * math.cos(x + i_fase[2][1])
+        frecuency = st.slider('Seleccione la frecuencia: ', 0.00, 100.00, 50.00)
 
-        rango = st.slider('Seleccione el valor maximo de x: ', 0.00, 50.00, 25.00)
-        step = 0.01
+        corriente_an = lambda x: i_fase[0][0] * math.cos(math.radians(frecuency * x + i_fase[0][1]))
+        corriente_bn = lambda x: i_fase[1][0] * math.cos(math.radians(frecuency * x + i_fase[1][1]))
+        corriente_cn = lambda x: i_fase[2][0] * math.cos(math.radians(frecuency * x + i_fase[2][1]))
+
+        rango = st.slider('Seleccione el valor maximo de x: ', 0.00, 100.00, 50.00)
+        step = 0.001
 
         i_an_x = np.arange(0, rango, step)
         i_an_y = [corriente_an(i) for i in i_an_x]
@@ -350,12 +373,15 @@ elif option == 'Estrella-Estrella':
         st.pyplot(fig)
 
     elif graphs == 'Corrientes de linea':
-        corriente_aa = lambda x: i_linea[0][0] * math.cos(x + i_linea[0][1])
-        corriente_bb = lambda x: i_linea[1][0] * math.cos(x + i_linea[1][1])
-        corriente_cc = lambda x: i_linea[2][0] * math.cos(x + i_linea[2][1])
 
-        rango = st.slider('Seleccione el valor maximo de x: ', 0.00, 50.00, 25.00)
-        step = 0.01
+        frecuency = st.slider('Seleccione la frecuencia: ', 0.00, 100.00, 50.00)
+
+        corriente_aa = lambda x: i_linea[0][0] * math.cos(math.radians(frecuency * x + i_linea[0][1]))
+        corriente_bb = lambda x: i_linea[1][0] * math.cos(math.radians(frecuency * x + i_linea[1][1]))
+        corriente_cc = lambda x: i_linea[2][0] * math.cos(math.radians(frecuency * x + i_linea[2][1]))
+
+        rango = st.slider('Seleccione el valor maximo de x: ', 0.00, 100.00, 50.00)
+        step = 0.001
 
         i_aa_x = np.arange(0, rango, step)
         i_aa_y = [corriente_aa(i) for i in i_aa_x]
@@ -378,12 +404,15 @@ elif option == 'Estrella-Estrella':
         st.pyplot(fig)
 
     elif graphs == 'Tensiones de fase':
-        tension_an = lambda x: v_fase[0][0] * math.cos(x + v_fase[0][1])
-        tension_bn = lambda x: v_fase[1][0] * math.cos(x + v_fase[1][1])
-        tension_cn = lambda x: v_fase[2][0] * math.cos(x + v_fase[2][1])
 
-        rango = st.slider('Seleccione el valor maximo de x: ', 0.00, 50.00, 25.00)
-        step = 0.01
+        frecuency = st.slider('Seleccione la frecuencia: ', 0.00, 100.00, 50.00)
+
+        tension_an = lambda x: v_fase[0][0] * math.cos(math.radians(frecuency * x + v_fase[0][1]))
+        tension_bn = lambda x: v_fase[1][0] * math.cos(math.radians(frecuency * x + v_fase[1][1]))
+        tension_cn = lambda x: v_fase[2][0] * math.cos(math.radians(frecuency * x + v_fase[2][1]))
+
+        rango = st.slider('Seleccione el valor maximo de x: ', 0.00, 100.00, 50.00)
+        step = 0.001
 
         v_an_x = np.arange(0, rango, step)
         v_an_y = [tension_an(i) for i in v_an_x]
@@ -406,12 +435,15 @@ elif option == 'Estrella-Estrella':
         st.pyplot(fig)
 
     else:
-        tension_an = lambda x: v_linea[0][0] * math.cos(x + v_linea[0][1])
-        tension_bn = lambda x: v_linea[1][0] * math.cos(x + v_linea[1][1])
-        tension_cn = lambda x: v_linea[2][0] * math.cos(x + v_linea[2][1])
 
-        rango = st.slider('Seleccione el valor maximo de x: ', 0.00, 50.00, 25.00)
-        step = 0.01
+        frecuency = st.slider('Seleccione la frecuencia: ', 0.00, 100.00, 50.00)
+
+        tension_an = lambda x: v_linea[0][0] * math.cos(math.radians(frecuency * x + v_linea[0][1]))
+        tension_bn = lambda x: v_linea[1][0] * math.cos(math.radians(frecuency * x + v_linea[1][1]))
+        tension_cn = lambda x: v_linea[2][0] * math.cos(math.radians(frecuency * x + v_linea[2][1]))
+
+        rango = st.slider('Seleccione el valor maximo de x: ', 0.00, 100.00, 50.00)
+        step = 0.001
 
         v_an_x = np.arange(0, rango, step)
         v_an_y = [tension_an(i) for i in v_an_x]
@@ -434,3 +466,227 @@ elif option == 'Estrella-Estrella':
         st.pyplot(fig)
 
 
+elif option == 'Delta-Estrella':
+
+    image = Image.open("DistribucionDeltaEstrella.png")
+    st.image(image, caption='Circuito trifasico Delta - Estrella')
+    
+    "## Selección voltajes:"
+
+    col1, col2, col3, col4= st.columns(4)
+
+    with col1:
+        v_ab_n = st.number_input('Magnitud $V_{ab}$', value=100)
+
+    with col2:
+        v_ab_a = st.number_input('Angulo $V_{ab}$', value=0)
+
+    with col3:
+        v_bc_n = st.number_input('Magnitud $V_{bc}$', value=100)
+
+    with col4:
+        v_bc_a = st.number_input('Angulo $V_{bc}$', value=-120)
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        v_ca_n = st.number_input('Magnitud $V_{ca}$', value=100)
+
+    with col2:
+        v_ca_a = st.number_input('Angulo de $V_{ca}$', value=-240)
+
+    "## Selección fasores:"
+
+    col1, col2, col3, col4= st.columns(4)
+
+    with col1:
+        z_an_r = st.number_input('Parte real de $Z_{an}$', value=40)
+
+    with col2:
+        z_an_i = st.number_input('Parte imaginaria de $Z_{an}$', value=15)
+
+    with col3:
+        z_bn_r = st.number_input('Parte real de $Z_{bn}$', value=35)
+
+    with col4:
+        z_bn_i = st.number_input('Parte imaginaria de $Z_{bn}$', value=-15)
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        z_cn_r = st.number_input('Parte real de $Z_{cn}$', value=10)
+
+    with col2:
+        z_cn_i = st.number_input('Parte imaginaria de $Z_{cn}$', value=30)
+
+    
+    v_ab: tuple = (v_ab_n, v_ab_a)
+    v_bc: tuple = (v_bc_n, v_bc_a)
+    v_ca: tuple = (v_ca_n, v_ca_a)
+
+    v: tuple = (v_ab, v_bc, v_ca)
+
+    z_an: complex = complex(z_an_r, z_an_i)
+    z_bn: complex = complex(z_bn_r, z_bn_i)
+    z_cn: complex = complex(z_cn_r, z_cn_i)
+
+    z: tuple = (z_an, z_bn, z_cn)
+
+    i_fase, i_linea, v_fase, v_linea = delta_estrella(v, z)
+
+    "----------------------------------------------------------------------------"
+
+
+    col1, col2 = st.columns(2)
+
+
+    with col1:
+        "### Corrientes de fase:"
+        st.markdown("$Ian$ = {:.3f} ∡ {:.3f}".format(i_fase[0][0], i_fase[0][1]))
+        st.markdown("$Ibn$ = {:.3f} ∡ {:.3f}".format(i_fase[1][0], i_fase[1][1]))
+        st.markdown("$Icn$ = {:.3f} ∡ {:.3f}".format(i_fase[2][0], i_fase[2][1]))
+
+        "### Corrientes de linea:"
+        st.markdown("$IAa$ = {:.3f} ∡ {:.3f}".format(i_linea[0][0], i_linea[0][1]))
+        st.markdown("$IBb$ = {:.3f} ∡ {:.3f}".format(i_linea[1][0], i_linea[1][1]))
+        st.markdown("$ICc$ = {:.3f} ∡ {:.3f}".format(i_linea[2][0], i_linea[2][1]))
+
+    with col2:
+        "### Tensiones de fase:"
+        st.markdown("$Van$ = {:.3f} ∡ {:.3f}".format(v_fase[0][0], v_fase[0][1]))
+        st.markdown("$Vbn$ = {:.3f} ∡ {:.3f}".format(v_fase[1][0], v_fase[1][1]))
+        st.markdown("$Vcn$ = {:.3f} ∡ {:.3f}".format(v_fase[2][0], v_fase[2][1]))
+
+        "### Tensiones de linea:"
+        st.markdown("$VAB$ = {:.3f} ∡ {:.3f}".format(v_linea[0][0], v_linea[0][1]))
+        st.markdown("$VBC$ = {:.3f} ∡ {:.3f}".format(v_linea[1][0], v_linea[1][1]))
+        st.markdown("$VCA$ = {:.3f} ∡ {:.3f}".format(v_linea[2][0], v_linea[2][1]))
+
+    "----------------------------------------------------------------------------"
+    graphs = st.selectbox(
+    'Graficns: ',
+    ('Corrientes de fase', 'Corrientes de linea', 'Tensiones de fase', 'Tensiones de linea'))
+
+    if graphs == 'Corrientes de fase':
+
+        frecuency = st.slider('Seleccione la frecuencia: ', 0.00, 100.00, 50.00)
+
+        corriente_an = lambda x: i_fase[0][0] * math.cos(math.radians(frecuency * x + i_fase[0][1]))
+        corriente_bn = lambda x: i_fase[1][0] * math.cos(math.radians(frecuency * x + i_fase[1][1]))
+        corriente_cn = lambda x: i_fase[2][0] * math.cos(math.radians(frecuency * x + i_fase[2][1]))
+
+        rango = st.slider('Seleccione el valor maximo de x: ', 0.00, 100.00, 50.00)
+        step = 0.001
+
+        i_an_x = np.arange(0, rango, step)
+        i_an_y = [corriente_an(i) for i in i_an_x]
+
+        i_bn_x = np.arange(0, rango, step)
+        i_bn_y = [corriente_bn(i) for i in i_bn_x]
+
+        i_cn_x = np.arange(0, rango, step)
+        i_cn_y = [corriente_cn(i) for i in i_cn_x]
+
+        fig, ax = plt.subplots()
+        ax.plot(i_an_x, i_an_y)
+        ax.plot(i_bn_x, i_bn_y)
+        ax.plot(i_cn_x, i_cn_y)
+        
+        plt.title('Corrientes de fase')
+        plt.legend(['Ian', 'Ibn', 'Icn'])
+        plt.grid(True)
+
+        st.pyplot(fig)
+
+    elif graphs == 'Corrientes de linea':
+
+        frecuency = st.slider('Seleccione la frecuencia: ', 0.00, 100.00, 50.00)
+
+        corriente_aa = lambda x: i_linea[0][0] * math.cos(math.radians(frecuency * x + i_linea[0][1]))
+        corriente_bb = lambda x: i_linea[1][0] * math.cos(math.radians(frecuency * x + i_linea[1][1]))
+        corriente_cc = lambda x: i_linea[2][0] * math.cos(math.radians(frecuency * x + i_linea[2][1]))
+
+        rango = st.slider('Seleccione el valor maximo de x: ', 0.00, 100.00, 50.00)
+        step = 0.001
+
+        i_aa_x = np.arange(0, rango, step)
+        i_aa_y = [corriente_aa(i) for i in i_aa_x]
+
+        i_bb_x = np.arange(0, rango, step)
+        i_bb_y = [corriente_bb(i) for i in i_bb_x]
+
+        i_cc_x = np.arange(0, rango, step)
+        i_cc_y = [corriente_cc(i) for i in i_cc_x]
+
+        fig, ax = plt.subplots()
+        ax.plot(i_aa_x, i_aa_y)
+        ax.plot(i_bb_x, i_bb_y)
+        ax.plot(i_cc_x, i_cc_y)
+
+        plt.title('Corrientes de linea')
+        plt.legend(['IAa', 'IBb', 'ICc'])
+        plt.grid(True)
+
+        st.pyplot(fig)
+
+    elif graphs == 'Tensiones de fase':
+
+        frecuency = st.slider('Seleccione la frecuencia: ', 0.00, 100.00, 50.00)
+
+        tension_an = lambda x: v_fase[0][0] * math.cos(math.radians(frecuency * x + v_fase[0][1]))
+        tension_bn = lambda x: v_fase[1][0] * math.cos(math.radians(frecuency * x + v_fase[1][1]))
+        tension_cn = lambda x: v_fase[2][0] * math.cos(math.radians(frecuency * x + v_fase[2][1]))
+
+        rango = st.slider('Seleccione el valor maximo de x: ', 0.00, 100.00, 50.00)
+        step = 0.001
+
+        v_an_x = np.arange(0, rango, step)
+        v_an_y = [tension_an(i) for i in v_an_x]
+
+        v_bn_x = np.arange(0, rango, step)
+        v_bn_y = [tension_bn(i) for i in v_bn_x]
+
+        v_cn_x = np.arange(0, rango, step)
+        v_cn_y = [tension_cn(i) for i in v_cn_x]
+
+        fig, ax = plt.subplots()
+        ax.plot(v_an_x, v_an_y)
+        ax.plot(v_bn_x, v_bn_y)
+        ax.plot(v_cn_x, v_cn_y)
+
+        plt.title('Tensiones de fase')
+        plt.legend(['Van', 'Vbn', 'Vcn'])
+        plt.grid(True)
+
+        st.pyplot(fig)
+
+    else:
+
+        frecuency = st.slider('Seleccione la frecuencia: ', 0.00, 100.00, 50.00)
+
+        tension_an = lambda x: v_linea[0][0] * math.cos(math.radians(frecuency * x + v_linea[0][1]))
+        tension_bn = lambda x: v_linea[1][0] * math.cos(math.radians(frecuency * x + v_linea[1][1]))
+        tension_cn = lambda x: v_linea[2][0] * math.cos(math.radians(frecuency * x + v_linea[2][1]))
+
+        rango = st.slider('Seleccione el valor maximo de x: ', 0.00, 100.00, 50.00)
+        step = 0.001
+
+        v_an_x = np.arange(0, rango, step)
+        v_an_y = [tension_an(i) for i in v_an_x]
+
+        v_bn_x = np.arange(0, rango, step)
+        v_bn_y = [tension_bn(i) for i in v_bn_x]
+
+        v_cn_x = np.arange(0, rango, step)
+        v_cn_y = [tension_cn(i) for i in v_cn_x]
+
+        fig, ax = plt.subplots()
+        ax.plot(v_an_x, v_an_y)
+        ax.plot(v_bn_x, v_bn_y)
+        ax.plot(v_cn_x, v_cn_y)
+
+        plt.title('Tensiones de linea')
+        plt.legend(['VAB', 'VBC', 'VCA'])
+        plt.grid(True)
+
+        st.pyplot(fig)
--- functions.py
+++ functions.py
@@ -28,19 +28,51 @@ def transform_to_rect(x: tuple) -> tuple:
 
 
 def fasors_d_to_y(z: tuple) -> tuple:
-    pass
+    z_ab: complex = z[0]
+    z_bc: complex = z[1]
+    z_ca: complex = z[2]
+
+    z_an: complex = z_ab * z_ca / (z_ab + z_bc + z_ca)
+    z_bn: complex = z_ab * z_bc / (z_ab + z_bc + z_ca)
+    z_cn: complex = z_bc * z_ca / (z_ab + z_bc + z_ca)
+
+    return (z_an, z_bn, z_cn)
 
 
 def fasors_y_to_d(z: tuple) -> tuple:
-    pass
+    z_an: complex = z[0]
+    z_bn: complex = z[1]
+    z_cn: complex = z[2]
+
+    z_ab: complex = ((z_an * z_bn) + (z_bn * z_cn) + (z_cn * z_an)) / z_cn
+    z_bc: complex = ((z_an * z_bn) + (z_bn * z_cn) + (z_cn * z_an)) / z_an
+    z_ca: complex = ((z_an * z_bn) + (z_bn * z_cn) + (z_cn * z_an)) / z_bn
+
+    return (z_ab, z_bc, z_ca)
 
 
 def voltages_d_to_y(v: tuple) -> tuple:
-    pass
+    v_ab = v[0]
+    v_bc = v[1]
+    v_ca = v[2]
+
+    v_an = (v_ab[0]/math.sqrt(3), v_ab - 30)
+    v_bn = (v_bc[0]/math.sqrt(3), v_bc - 30)
+    v_cn = (v_ca[0]/math.sqrt(3), v_ca - 30)
+
+    return (v_an, v_bn, v_cn)
 
 
 def voltages_y_to_d(v: tuple) -> tuple:
-    pass
+    v_an = v[0]
+    v_bn = v[1]
+    v_cn = v[2]
+
+    v_ab = (math.sqrt(3)*v_an[0], v_an[1] + 30)
+    v_bc = (math.sqrt(3)*v_bn[0], v_bn[1] + 30)
+    v_ca = (math.sqrt(3)*v_cn[0], v_cn[1] + 30)
+
+    return (v_ab, v_bc, v_ca)
 
 
 # Simulador casos circuitos trifasicos:
@@ -169,3 +201,156 @@ def estrella_estrella(v: tuple, z: tuple) -> None:
     print("VCA = {:.3f} ∡ {:.3f}".format(f_ca[0], f_ca[1]))
 
     return (i_an, i_bn, i_cn), (i_an, i_bn, i_cn), (f_an, f_bn, f_cn), (f_ab, f_bc, f_ca)
+
+
+# Delta - Estrella
+
+def delta_estrella(v: tuple, z: tuple) -> None:
+    # Fuentes
+    f_ab: tuple = v[0]
+    f_bc: tuple = v[1]
+    f_ca: tuple = v[2]
+
+    f_pol: tuple = (f_ab, f_bc, f_ca)
+    f_rect: tuple = transform_to_rect(f_pol)
+
+    # Fasores
+    z_an: complex = z[0]
+    z_bn: complex = z[1]
+    z_cn: complex = z[2]
+
+    z_rect: tuple = (z_an, z_bn, z_cn)
+    z_pol: tuple = transform_to_polar(z_rect)
+
+    z_rect_d: tuple = fasors_y_to_d(z_rect) # Transforma los fasores a distribución delta para enccontrar las corrientes de linea.
+
+    # Corrientes de linea / fase
+    i_ab_d: complex = f_rect[0] / z_rect_d[0]
+    i_bc_d: complex = f_rect[1] / z_rect_d[1]
+    i_ca_d: complex = f_rect[2] / z_rect_d[2]
+
+    i_aa: tuple = transform_to_polar((i_ab_d - i_ca_d, ))[0]
+    i_bb: tuple = transform_to_polar((i_bc_d - i_ab_d, ))[0]
+    i_cc: tuple = transform_to_polar((i_ca_d - i_bc_d, ))[0]
+
+    i_an: tuple = i_aa
+    i_bn: tuple = i_bb
+    i_cn: tuple = i_cc
+
+    # Tensiones de linea
+    f_ab : tuple = f_pol[0]
+    f_bc : tuple = f_pol[1]
+    f_ca : tuple = f_pol[2]
+
+    # Tensiones de fase
+    v_an = transform_to_polar((transform_to_rect((i_an, ))[0] * z_rect[0], ))[0]
+    v_bn = transform_to_polar((transform_to_rect((i_bn, ))[0] * z_rect[1], ))[0]
+    v_cn = transform_to_polar((transform_to_rect((i_cn, ))[0] * z_rect[2], ))[0]
+
+
+
+    # Print solution
+    print("Corrientes de fase:")
+    print("Ian = {:.3f} ∡ {:.3f}".format(i_an[0], i_an[1]))
+    print("Ibn = {:.3f} ∡ {:.3f}".format(i_bn[0], i_bn[1]))
+    print("Icn = {:.3f} ∡ {:.3f}".format(i_cn[0], i_cn[1]))
+
+    print("")
+
+    print("Corrientes de linea:")
+    print("IAa = {:.3f} ∡ {:.3f}".format(i_aa[0], i_aa[1]))
+    print("IBb = {:.3f} ∡ {:.3f}".format(i_bb[0], i_bb[1]))
+    print("ICc = {:.3f} ∡ {:.3f}".format(i_cc[0], i_cc[1]))
+
+    print("")
+
+    print("Tensiones de fase:")
+    print("Van = {:.3f} ∡ {:.3f}".format(v_an[0], v_an[1]))
+    print("Vbn = {:.3f} ∡ {:.3f}".format(v_bn[0], v_bn[1]))
+    print("Vcn = {:.3f} ∡ {:.3f}".format(v_cn[0], v_cn[1]))
+
+    print("")
+
+    print("Tensiones de linea:")
+    print("VAB = {:.3f} ∡ {:.3f}".format(f_ab[0], f_ab[1]))
+    print("VBC = {:.3f} ∡ {:.3f}".format(f_bc[0], f_bc[1]))
+    print("VCA = {:.3f} ∡ {:.3f}".format(f_ca[0], f_ca[1]))
+
+
+    return (i_an, i_bn, i_cn), (i_aa, i_bb, i_cc), (v_an, v_bn, v_cn), (f_ab, f_bc, f_ca)
+
+
+#  Estrella - Delta
+
+def estrella_delta(v: tuple, z: tuple) -> None:
+    # Fuentes / Tensiones fase
+    f_an: tuple = v[0]
+    f_bn: tuple = v[1]
+    f_cn: tuple = v[2]
+
+    f_pol: tuple = (f_an, f_bn, f_cn)
+    f_rect: tuple = transform_to_rect(f_pol)
+
+    f_pol_d: tuple = voltages_y_to_d(f_pol)
+    f_rect_d: tuple = transform_to_rect(f_pol_d)
+
+    # Fasores
+    z_ab: complex = z[0]
+    z_bc: complex = z[1]
+    z_ca: complex = z[2]
+
+    z_rect: tuple = (z_ab, z_bc, z_ca)
+    z_pol: tuple = transform_to_polar(z_rect)
+
+    # Corrientes de linea
+    i_ab_d: complex = f_rect_d[0] / z_rect[0]
+    i_bc_d: complex = f_rect_d[1] / z_rect[1]
+    i_ca_d: complex = f_rect_d[2] / z_rect[2]
+
+    i_aa: tuple = transform_to_polar((i_ab_d - i_ca_d, ))[0]
+    i_bb: tuple = transform_to_polar((i_bc_d - i_ab_d, ))[0]
+    i_cc: tuple = transform_to_polar((i_ca_d - i_bc_d, ))[0]
+
+    # Tensiones de linea
+    f_AB : tuple = transform_to_polar((f_rect[0] - f_rect[1], ))[0]
+    f_BC : tuple = transform_to_polar((f_rect[1] - f_rect[2], ))[0]
+    f_CA : tuple = transform_to_polar((f_rect[2] - f_rect[0], ))[0]
+
+    # Tensiones de fase
+    f_ab: tuple = transform_to_polar((f_rect[1] - f_rect[0], ))[0]
+    f_bc: tuple = transform_to_polar((f_rect[2] - f_rect[1], ))[0] 
+    f_ca: tuple = transform_to_polar((f_rect[2] - f_rect[0], ))[0]
+
+    # Corrientes de fase
+    i_ab: tuple = transform_to_polar((transform_to_rect((f_ab, ))[0] / z_rect[0], ))[0]
+    i_bc: tuple = transform_to_polar((transform_to_rect((f_bc, ))[0] / z_rect[1], ))[0]
+    i_ca: tuple = transform_to_polar((transform_to_rect((f_ca, ))[0] / z_rect[2], ))[0]
+
+    # Print solution
+    print("Corrientes de fase:")
+    print("Ian = {:.3f} ∡ {:.3f}".format(i_ab[0], i_ab[1]))
+    print("Ibn = {:.3f} ∡ {:.3f}".format(i_bc[0], i_bc[1]))
+    print("Icn = {:.3f} ∡ {:.3f}".format(i_ca[0], i_ca[1]))
+
+    print("")
+
+    print("Corrientes de linea:")
+    print("IAa = {:.3f} ∡ {:.3f}".format(i_aa[0], i_aa[1]))
+    print("IBb = {:.3f} ∡ {:.3f}".format(i_bb[0], i_bb[1]))
+    print("ICc = {:.3f} ∡ {:.3f}".format(i_cc[0], i_cc[1]))
+
+    print("")
+
+    print("Tensiones de fase:")
+    print("Van = {:.3f} ∡ {:.3f}".format(f_ab[0], f_ab[1]))
+    print("Vbn = {:.3f} ∡ {:.3f}".format(f_bc[0], f_bc[1]))
+    print("Vcn = {:.3f} ∡ {:.3f}".format(f_ca[0], f_ca[1]))
+
+    print("")
+
+    print("Tensiones de linea:")
+    print("VAB = {:.3f} ∡ {:.3f}".format(f_AB[0], f_AB[1]))
+    print("VBC = {:.3f} ∡ {:.3f}".format(f_BC[0], f_BC[1]))
+    print("VCA = {:.3f} ∡ {:.3f}".format(f_CA[0], f_CA[1]))
+
+    return (i_ab, i_bc, i_ca), (i_aa, i_bb, i_cc), (f_ab, f_bc, f_ca), (f_AB, f_BC, f_CA)
